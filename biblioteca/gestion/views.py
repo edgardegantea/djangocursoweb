38c0c7d50f2f38004e0f94e11066f9de57ac6fc4
edgardegantea <edgar.degante.a@gmail.com>
2023-04-24 16:40:40 -0500

    Actualización de templates en Django

--- biblioteca/gestion/views.py
+++ biblioteca/gestion/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'gestion/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        return context
+
+
+class AcercaDe(TemplateView):
+    template_name = 'gestion/acercade.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AcercaDe, self).get_context_data(**kwargs)
+        return context
+
+
